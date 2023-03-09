3859340dd5c3b254af9931874e34185dafd04e5d
adhamsuliman <adhamsuliman93@gmail.com>
2023-03-08 21:32:02 -0500

    adding instappy.py

--- doa/instappy.py
+++ doa/instappy.py
@@ -21,15 +21,14 @@ set_workspace(path=None)
 def job():
   session = InstaPy(username=insta_username, password=insta_password)
   with smart_run(session):
-
+    session.set_smart_location_hashtags(['215069212','217184762'], radius=50)
     session.set_do_comment(enabled=True, percentage=20)
-    session.set_comments(['Well done!'])
+    session.set_comments(['Ramadan Kareem! @{}'], media='Photo')
     session.set_do_follow(enabled=True, percentage=5, times=2)
-    session.like_by_tags(['love'], amount=100, media='Photo')
-
+    session.set_mandatory_words(['#ramadan', '#instafood'])
+    session.like_by_tags(['رمضان'], amount=100, media='Photo', use_smart_location_hashtags=True) #'ramadan', 'Ramadan',
 
 schedule.every().hour.do(job)
-
 
 while True:
   schedule.run_pending()
