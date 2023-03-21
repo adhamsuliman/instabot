8843125abcce41903bf3ab3a72b5952d1c0e416b
adhamsuliman <adhamsuliman93@gmail.com>
2023-03-21 18:24:54 -0400

    a whole lotta changes

--- doa/instappy.py
+++ doa/instappy.py
@@ -3,7 +3,7 @@ from instapy import smart_run
 from instapy import set_workspace
 import schedule
 import time
-from dotenv import load_dotenv,find_dotenv
+from dotenv import load_dotenv, find_dotenv
 import os
 
 load_dotenv()
@@ -11,25 +11,27 @@ CHALLENGE_EMAIL = os.environ['CHALLENGE_EMAIL']
 CHALLENGE_PASSWORD = os.environ['CHALLENGE_PASSWORD']
 IG_USERNAME = os.environ['IG_USERNAME']
 IG_PASSWORD = os.environ['IG_PASSWORD']
-#your login credentials
-insta_username=IG_USERNAME
-insta_password=IG_PASSWORD 
+# your login credentials
+insta_username = IG_USERNAME
+insta_password = IG_PASSWORD
 
-#path to your workspace
+# path to your workspace
 set_workspace(path=None)
 
-def job():
-  session = InstaPy(username=insta_username, password=insta_password)
-  with smart_run(session):
-    session.set_smart_location_hashtags(['215069212','217184762'], radius=50)
-    session.set_do_comment(enabled=True, percentage=20)
-    session.set_comments(['Ramadan Kareem! @{}'], media='Photo')
-    session.set_do_follow(enabled=True, percentage=5, times=2)
-    session.set_mandatory_words(['#ramadan', '#instafood'])
-    session.like_by_tags(['رمضان'], amount=100, media='Photo', use_smart_location_hashtags=True) #'ramadan', 'Ramadan',
 
-schedule.every().hour.do(job)
+def job():
+    session = InstaPy(username=insta_username, password=insta_password, want_check_browser=False)
+    with smart_run(session):
+        session.set_smart_location_hashtags(['215069212', '217184762'], radius=50)
+        session.set_do_comment(enabled=True, percentage=20)
+        session.set_comments(['Ramadan Kareem! @{}'], media='Photo')
+        session.set_do_follow(enabled=True, percentage=5, times=2)
+        session.set_mandatory_words(['#ramadan', '#instafood'])
+        session.like_by_tags(['رمضان'], amount=100, media='Photo', use_smart_location_hashtags=True)  # 'ramadan', 'Ramadan',
+
+
+schedule.every(1).second.do(job)
 
 while True:
-  schedule.run_pending()
-  time.sleep(10)
+    schedule.run_pending()
+    time.sleep(10)
