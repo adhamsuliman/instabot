d669d107f4dc4472fdca0a9bc81cc58c6599750f
adhamsuliman <adhamsuliman93@gmail.com>
2023-03-21 22:15:02 -0400

    randomly run every 10 minutes with 1/3

--- pad/instappy.py
+++ pad/instappy.py
@@ -1,3 +1,5 @@
+import random
+
 from instapy import InstaPy
 from instapy import smart_run
 from instapy import set_workspace
@@ -19,19 +21,34 @@ insta_password = IG_PASSWORD
 set_workspace(path=None)
 
 
-def job():
-    session = InstaPy(username=insta_username, password=insta_password, headless_browser=True, want_check_browser=False)
-    with smart_run(session):
-        # session.set_smart_location_hashtags([(13.7563,100.5018)], radius=50)
-        session.set_do_comment(enabled=True, percentage=20)
-        session.set_comments(["That's a great look Pad Thai! @{}"], media='Photo')
-        session.set_do_follow(enabled=True, percentage=5, times=2)
-        # session.set_mandatory_words(['#ramadan', '#instafood'])
-        session.like_by_tags(['padthai', 'PadThai','Padthai'], amount=100, media='Photo')  # 'ramadan', 'Ramadan', , use_smart_location_hashtags=True
+class Instapy_bot:
+    def __init__(self):
+        CHALLENGE_EMAIL = os.environ['CHALLENGE_EMAIL']
+        CHALLENGE_PASSWORD = os.environ['CHALLENGE_PASSWORD']
+        IG_USERNAME = os.environ['IG_USERNAME']
+        IG_PASSWORD = os.environ['IG_PASSWORD']
+        self.session = InstaPy(username=IG_USERNAME, password=IG_PASSWORD, headless_browser=True, want_check_browser=False)
+
+    def run(self):
+        with smart_run(self.session):
+            # session.set_smart_location_hashtags([(13.7563,100.5018)], radius=50)
+            self.session.set_do_comment(enabled=True, percentage=20)
+            self.session.set_comments(["That's a great look Pad Thai! @{}"], media='Photo')
+            self.session.set_do_follow(enabled=True, percentage=5, times=2)
+            # session.set_mandatory_words(['#ramadan', '#instafood'])
+            self.session.like_by_tags(['padthai', 'PadThai', 'Padthai'], amount=100,
+                                      media='Photo')  # 'ramadan', 'Ramadan', , use_smart_location_hashtags=True
+
+    def job(self):
+        for i in range(3):
+            delay = random.randint(1, 60 * 60 // 3)
+            self.schedule.enter(delay, 1, self.run())
+        self.schedule.run()
 
 
-schedule.every(1).second.do(job)
-
-while True:
-    schedule.run_pending()
-    time.sleep(10)
+if __name__ == '__main__':
+    bot = Instapy_bot()
+    while True:
+        if random.random() < 1 / 3:
+            bot.run()
+            time.sleep(1)
