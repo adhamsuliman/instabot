30977db1bb22824e649dd80e0179a2d28c8727dd
adhamsuliman <adhamsuliman93@gmail.com>
2023-03-21 21:58:20 -0400

    adding instapy for padthai

---
+++ pad/instappy.py
@@ -0,0 +1,37 @@
+from instapy import InstaPy
+from instapy import smart_run
+from instapy import set_workspace
+import schedule
+import time
+from dotenv import load_dotenv, find_dotenv
+import os
+
+load_dotenv()
+CHALLENGE_EMAIL = os.environ['CHALLENGE_EMAIL']
+CHALLENGE_PASSWORD = os.environ['CHALLENGE_PASSWORD']
+IG_USERNAME = os.environ['IG_USERNAME']
+IG_PASSWORD = os.environ['IG_PASSWORD']
+# your login credentials
+insta_username = IG_USERNAME
+insta_password = IG_PASSWORD
+
+# path to your workspace
+set_workspace(path=None)
+
+
+def job():
+    session = InstaPy(username=insta_username, password=insta_password, want_check_browser=False)
+    with smart_run(session):
+        session.set_smart_location_hashtags([(13.7563,100.5018)], radius=50)
+        session.set_do_comment(enabled=True, percentage=20)
+        session.set_comments(["That's a great look Pad Thai! @{}"], media='Photo')
+        session.set_do_follow(enabled=True, percentage=5, times=2)
+        # session.set_mandatory_words(['#ramadan', '#instafood'])
+        session.like_by_tags(['padthai', 'PadThai','Padthai'], amount=100, media='Photo', use_smart_location_hashtags=True)  # 'ramadan', 'Ramadan',
+
+
+schedule.every(1).second.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(10)
